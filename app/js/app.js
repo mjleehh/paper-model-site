209de517d566a7c41581f89b4a4b7092ead699ce
Michael Jonathan Lee <code.mjlee@gmail.com>
2015-03-11 00:44:29 +0100

    Connected paper model library and run conversion stub.

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,6 @@
 var $ = require('jquery');
+var _ = require('lodash');
+var paperModelLib = require('paper-model');
 
 
 var sections = [];
@@ -88,6 +90,21 @@ Section.prototype.alignSection = function(){
         left: newValue
     });
 };
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+$('#fileupload').on('change', function(event){
+    var file = _.first(event.target.files);
+    var reader = new FileReader();
+    reader.onloadend = function(){
+        var mesh = paperModelLib.mesh.readObj(reader.result);
+        var result = paperModelLib.mesh.writeObj(paperModelLib.createPaperModel(mesh));
+        console.log(result);
+        //result = result.replace(/\n/g, ' <br> ');
+        //$('.result-area').text(result);
+    };
+    reader.readAsText(file);
+});
 
 // ---------------------------------------------------------------------------------------------------------------------
 
